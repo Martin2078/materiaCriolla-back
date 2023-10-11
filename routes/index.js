f9197ae650843d1e5c1fac81c200c05792ea6418
Martin <martinleonelgalan@gmail.com>
2023-10-11 11:12:25 -0300

    app

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 import {Router} from 'express';
 import productsRouter from './products.js'
 import categoryRouter from './category.js';
-const router = Router();
-import express from 'express';
 import userRouter from "./users.js"
-const router = express.Router();
+
+const router = Router();
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.use("/auth", userRouter)
-
 router.use("/products", productsRouter)
 router.use("/categorys",categoryRouter)
 
