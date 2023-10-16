d5143f9f04fd061c53cdad5571eb2c0ccd8dce79
Santiago <santiagonlaghi@hotmail.com>
2023-10-16 10:01:58 -0300

    .

--- controllers/auth/signIn.js
+++ controllers/auth/signIn.js
@@ -18,7 +18,9 @@ export default async function (req, res) {
         } })
 
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ error: 'Error en el servidor' });
+        
     }
 };
 
