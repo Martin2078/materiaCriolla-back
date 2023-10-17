21ab7258e8d512bf06ed552cd269a4222bcad7d1
Santiago <santiagonlaghi@hotmail.com>
2023-10-17 00:31:03 -0300

    trayendo mp

--- middlewares/findUser.js
+++ middlewares/findUser.js
@@ -5,7 +5,7 @@ export default async function (req,res,next) {
     try {
         const finded = await User.findOne({ email:req.body.email })
         if (!finded) {
-            return res.status(401).json({ error: 'Email incorrecto'});
+            return res.status(401).json({ error: 'Wrong email'});
         }
         return next()
 
